832526998a9263a323fd081d0cdcbc73ae5ac26c
LDprg <lukas_4dr@gmx.at>
2023-10-09 16:03:55 +0200

    fix code even more

--- custom_components/knv_heatpump/__init__.py
+++ custom_components/knv_heatpump/__init__.py
@@ -15,13 +15,12 @@ from __future__ import annotations
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 
-# The domain of your component. Should be equal to the name of your component.
 DOMAIN = "knv_heatpump"
 
-def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+
+async def async_setup(hass: HomeAssistant, _config: ConfigType):
     """Setup our skeleton component."""
-    # States are in the format DOMAIN.OBJECT_ID.
+
     hass.states.async_set('knv_heatpump.Hello_World', 'Works!')
 
-    # Return boolean to indicate that initialization was successfully.
     return True
