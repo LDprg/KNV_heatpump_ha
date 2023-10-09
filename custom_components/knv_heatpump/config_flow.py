998c9d73f2d26e82b2843a9efce47bc05badc1df
LDprg <lukas_4dr@gmx.at>
2023-10-09 16:36:56 +0200

    try setup

--- custom_components/knv_heatpump/config_flow.py
+++ custom_components/knv_heatpump/config_flow.py
@@ -13,10 +13,13 @@ class KnvHeatpumpFlow(config_entries.ConfigFlow, domain=knv.DOMAIN):
     """
     KNV heatpump config flow
     """
-    async def async_step_user(self, info):
+    async def async_step_init(self, info):
+        """
+        Init step
+        """
         if info is not None:
             pass
 
         return self.async_show_form(
-            step_id="user", data_schema=vol.Schema({vol.Required("password"): str})
+            step_id="init", data_schema=vol.Schema({vol.Required("host"): str})
         )
