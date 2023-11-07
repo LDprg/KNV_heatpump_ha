ff7dc20468d5dafe56315ac23b3205e8b753e1ae
LDprg <lukas_4dr@gmx.at>
2023-11-07 16:30:21 +0100

    fix typo

--- custom_components/knv_heatpump/__init__.py
+++ custom_components/knv_heatpump/__init__.py
@@ -37,7 +37,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     )
 
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "numbers")
+        hass.config_entries.async_forward_entry_setup(entry, "number")
     )
 
     return True
