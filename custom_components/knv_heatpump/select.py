e796bf49bdfff99d77139f06286173ec6fefe2ff
LDprg <lukas_4dr@gmx.at>
2023-11-08 11:00:18 +0100

    update version

--- custom_components/knv_heatpump/select.py
+++ custom_components/knv_heatpump/select.py
@@ -52,9 +52,8 @@ class KnvSelect(CoordinatorEntity, SelectEntity):
             self._attr_name = self.data["path"] + " - " + self.data["name"]
             self._attr_unique_id = self.data["path"]
 
-            if "listentries" in self.data:
-                for data in self.data["listentries"]:
-                    self._attr_options.append(data["text"])
+            for data in self.data["listentries"]:
+                self._attr_options.append(data["text"])
 
     @callback
     def _handle_coordinator_update(self) -> None:
