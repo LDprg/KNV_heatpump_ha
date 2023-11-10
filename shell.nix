ab972684f4d7543133c11ee27a010372f3e2ef26
LDprg <lukas_4dr@gmx.at>
2023-11-10 10:05:51 +0100

    update

--- shell.nix
+++ shell.nix
@@ -37,7 +37,7 @@ pkgs.mkShell {
           })
           (buildPythonPackage rec {
             pname = "knvheatpumplib";
-            version = "0.0.27";
+            version = "0.0.28";
             pyproject = true;
             doCheck = false;
 
