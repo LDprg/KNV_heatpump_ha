80aa7085b158517b969130fe067bf4ccfa321a4d
LDprg <lukas_4dr@gmx.at>
2024-05-20 11:50:34 +0200

    Work around writeable problem

--- custom_components/knv_heatpump/const.py
+++ custom_components/knv_heatpump/const.py
@@ -17,7 +17,7 @@ class Type(Enum):
 
 
 def getType(data):
-    if data["writeable"] is True:
+    if data.get("writeable") is True:
         if data["type"] == 4 or data["type"] == 6 or data["type"] == 8:
             return Type.NUMBER
 
