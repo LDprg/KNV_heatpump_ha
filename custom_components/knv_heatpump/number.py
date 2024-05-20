fce6cf1914ea84f65b8623076d3b5dba24c71aff
LDprg <lukas_4dr@gmx.at>
2024-05-20 20:44:38 +0200

    Revert "add logging"
    This reverts commit 565b299d476e97b49275d79357f37fdee1501641.

--- custom_components/knv_heatpump/number.py
+++ custom_components/knv_heatpump/number.py
@@ -28,7 +28,6 @@ async def async_setup_entry(
     def _async_measurement_listener() -> None:
         """Listen for new measurements and add sensors if they did not exist."""
         for path in coordinator.data:
-            knv.LOGGER.info("Select: %s", path)
             if knv.getType(coordinator.data[path]) == knv.Type.NUMBER:
                 if not path in coordinator.paths:
                     coordinator.paths.append(path)
